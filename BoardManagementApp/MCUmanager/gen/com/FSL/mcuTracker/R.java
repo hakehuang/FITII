26430d54bec70ad8c4108535c5782667e0d40fc0
JingyiGao <faygao52@gmail.com>
2014-12-16 14:17:30 +0800

    change icon of app

--- BoardManagementApp/MCUmanager/gen/com/FSL/mcuTracker/R.java
+++ BoardManagementApp/MCUmanager/gen/com/FSL/mcuTracker/R.java
@@ -1859,11 +1859,12 @@ containing a value of this type.
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
         public static final int ic_launcher=0x7f020033;
-        public static final int mm_title_back_btn=0x7f020034;
-        public static final int mm_title_back_focused=0x7f020035;
-        public static final int mm_title_back_normal=0x7f020036;
-        public static final int mm_title_back_pressed=0x7f020037;
-        public static final int mmtitle_bg_alpha=0x7f020038;
+        public static final int icon=0x7f020034;
+        public static final int mm_title_back_btn=0x7f020035;
+        public static final int mm_title_back_focused=0x7f020036;
+        public static final int mm_title_back_normal=0x7f020037;
+        public static final int mm_title_back_pressed=0x7f020038;
+        public static final int mmtitle_bg_alpha=0x7f020039;
     }
     public static final class id {
         public static final int SearchView=0x7f0a0077;
